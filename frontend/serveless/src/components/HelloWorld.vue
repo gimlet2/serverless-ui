7ed1392b35a36ae16004ad418f1b1a98eb7baa35
Andrei Chernyshev <andrei.chernyshev@outfittery.de>
2018-10-11 15:48:37 +0200

    add material ui

--- frontend/serveless/src/components/HelloWorld.vue
+++ frontend/serveless/src/components/HelloWorld.vue
@@ -1,22 +1,43 @@
 <template>
-  <div  class="row">
-    <div class="column left">
-      <ul>
-        <li v-for="v in functions" v-bind:key="v.metadata.name">
-          <a @click="functionFiles(v.metadata.namespace, v.metadata.name)"> {{ v.metadata.name}}</a>
-          <ul v-if="selectedFunction == v.metadata.name">
-            <li v-for="f in files" v-bind:key="f">
-              <a @click="loadFile(v.metadata.namespace, v.metadata.name, f)"> {{f}}</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-    <div class="column right">
+
+  <md-app>
+    <md-app-toolbar class="md-primary">
+      <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <span class="md-title">Kubeless UI</span>
+    </md-app-toolbar>
+
+    <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <span>Functions</span>
+
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button md-dense" @click="toggleMenu">
+            <md-icon>keyboard_arrow_left</md-icon>
+          </md-button>
+        </div>
+      </md-toolbar>
+
+      <md-list :md-expand-single="true">
+        <md-list-item v-for="v in functions" v-bind:key="v.metadata.name" md-expand :md-expanded.sync="expandFunctions[v.metadata.name]">
+          <a class="md-list-item-text" @click="functionFiles(v.metadata.namespace, v.metadata.name)"> {{ v.metadata.name}}</a>
+          <md-list slot="md-expand">
+            <md-list-item  class="md-inset" v-for="f in files" v-bind:key="f">
+              <a class="md-list-item-text" @click="loadFile(v.metadata.namespace, v.metadata.name, f)"> {{f}}</a>
+            </md-list-item>
+          </md-list>
+        </md-list-item>
+
+      </md-list>
+    </md-app-drawer>
+
+    <md-app-content>
       <Monaco width="100%" height="800" :language="language" theme="vs-dark" :code="code || ''" :changeThrottle="500" @codeChange="(e) => code = e.getValue()" :options="{automaticLayout: true, wordWrap: 'on', autoIndent: true, folding: true}">
       </Monaco>
-    </div>
-  </div>
+    </md-app-content>
+  </md-app>
+
 </template>
 
 <script>
@@ -30,6 +51,9 @@ export default {
   },
   data() {
     return {
+      menuVisible: false,
+      expandSingle: false,
+      expandFunctions: {},
     code: '',
     functions: [],
     files: [],
@@ -43,6 +67,9 @@ export default {
     },
 
   methods: {
+    toggleMenu () {
+        this.menuVisible = !this.menuVisible
+      },
     functionFiles(ns, name) {
       this.selectedFunction = name;
       new FunctionProxy().files(ns, name).then((r) => {
@@ -66,6 +93,9 @@ export default {
   mounted() {
     new FunctionProxy().list().then((r) => {
         this.functions = r.items;
+        this.functions.forEach(e => {
+          this.expandFunctions[e.metadata.name] = false;  
+        })
     });
   }
 }
@@ -74,7 +104,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .row {
-    display: flex;
+  display: flex;
 }
 .left {
   width: 500px;
